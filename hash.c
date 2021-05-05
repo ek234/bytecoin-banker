c8b8286162b4d2d84d96d935b8b3ddc951ac413e
IshanyaSethi <76902436+IshanyaSethi@users.noreply.github.com>
2021-05-05 14:38:06 +0530

    updated hash function

--- hash.c
+++ hash.c
@@ -2,13 +2,12 @@
 #include "trans.h"
 #include "update_val.h"
 
-
+struct Hashkey hashkey;
 ElemType Hash(Block B,Transact T){
-    int hashval,hashval_2;
-    hashval = ((B->Nonce)%B->block_num);
-    hashval_2 = T->S_UID + T->R_UID + T->tr_amount;
-    hashval = hashval << (hashval_2 % 2);
-    B->hash_val = hashval+ B->prev_block_hash;
+    hashkey.hashval = B->Nonce;
+    hashkey.hashval_2 = (T->S_UID + T->R_UID + T->tr_amount)%B->block_num;
+    hashkey.hashval = hashkey.hashval << (hashkey.hashval_2 % 2);
+    B->hash_val = hashkey.hashval+ B->prev_block_hash;
     return B->hash_val;
 }
 
