0258a50a2cb37f3ea9ef40debf8b01de6ee5a65b
hitesh05 <76490422+hitesh05@users.noreply.github.com>
2021-05-06 13:52:23 +0530

    removed parameter Transact T

--- hash.c
+++ hash.c
@@ -3,11 +3,13 @@
 #include "update_val.h"
 
 struct Hashkey hashkey;
-ElemType Hash(Block B,Transact T){
+
+ElemType Hash(Block B)
+{
     hashkey.hashval = B->Nonce;
-    hashkey.hashval_2 = (T->S_UID + T->R_UID + T->tr_amount)%B->block_num;
+    hashkey.hashval_2 = (B->T->next->S_UID + B->T->next->R_UID + B->T->next->tr_amount) % B->block_num; //error
     hashkey.hashval = hashkey.hashval << (hashkey.hashval_2 % 2);
-    B->hash_val = hashkey.hashval+ B->prev_block_hash;
+    B->hash_val = hashkey.hashval + B->prev_block_hash;
     return B->hash_val;
 }
 
