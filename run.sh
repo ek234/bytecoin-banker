8951be1ca7abd4acdab57252e673bf00a56ddc14
rishabh770 <rishabh.agrawal@students.iiit.ac.in>
2021-05-08 20:23:57 +0530

    Finished README

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
  #!/bin/bash
 
- gcc -g main.c bitcoin.c hash.c trans.c user.c update_val.c -o bitcoin.out && ./bitcoin.out
+ gcc main.c bitcoin.c hash.c trans.c user.c update_val.c -o bitcoin.out && ./bitcoin.out
