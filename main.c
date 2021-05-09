4937cdf1b511e54d7b4e94805d3f3c78f0309063
ek234 <71319388+ek234@users.noreply.github.com>
2021-05-09 16:38:08 +0530

    made main.h to clean main.c

--- main.c
+++ main.c
@@ -1,46 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-#include <assert.h>
-#include "./bitcoin.h"
-#include "./user.h"
-#include "./trans.h"
-#include "./update_val.h"
-#include "./color.h"
+#include "./main.h"
 
-#define APP_NAME "iiitcoin"
-#define command_length 50
-#define trans_per_block 50
-
-void printhelp()
-{
-	// help page
-	Bcyan();
-	printf("\n\t\t\tWe are here to help you!\n");
-	reset();
-	cyan();
-	printf("Instructions: -\nUse the following commands to move ahead:\n");
-	reset();
-	printf("1.\tPress 'attack' to see your situation with the equity (Attack).\n");
-	printf("2.\tPress 'balance' to check the balance no. of bitcoins.\n");
-	printf("3.\tPress 'check' to check the value of bitcoin with respect to $.\n");
-	printf("4.\tPress 'help' to return to the set of instructions.\n");
-	printf("5.\tPress 'past' to display upto the last 5 transaction of a user\n");
-	printf("6.\tPress 'mine' to mine bitcoins.\n");
-	printf("7.\tPress 'register' to register your details.\n");
-	printf("8.\tPress 'transfer' to fill your transaction details.\n");
-	printf("9.\tPress 'unregister' to remove your details from the list.\n");
-	printf("10.\tPress 'validity' to check the validity of your Block Chain.\n");
-	printf("11.\tPress 'exit' to exit from the page.\n");
-	printf("Note: in %s v0.5+, users can select only the first alphabet of the command.\n", APP_NAME);
-	printf("\n%s v0.6.01\n", APP_NAME);
-	cyan();
-	printf("\n\t\t\tHave a smooth Experience here. Stay Safe🌻\n");
-	reset();
-}
-	   
-	       
 int main()
 {
 
@@ -76,8 +35,8 @@ int main()
     net_data.new_trans = 0;
 //
 	cyan();
-	printf("\n\t\t\t\t🙏NAMASKAAR🙏\n\n");
-	printf("\n\t\t\t    Welcome to %s\n\n", APP_NAME);
+	printf("\n\t\t\t\t🙏NAMASKAAR🙏\n");
+	printf("\n\t\t\t    Welcome to %s\n", APP_NAME);
 	reset();
 	printhelp();
 
