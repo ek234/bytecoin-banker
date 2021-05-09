9b1fce22727d27eeeafda8c92f6317a7dd0160a9
anjalish05 <77845126+anjalish05@users.noreply.github.com>
2021-05-09 14:36:27 +0530

    Update main.c

--- main.c
+++ main.c
@@ -7,41 +7,11 @@
 #include "./user.h"
 #include "./trans.h"
 #include "./update_val.h"
+#include "./color.h"
 
 #define APP_NAME "iiitcoin"
 #define command_length 50
 #define trans_per_block 50
-
-void Bblack(){
-	printf("\033[1;30m");
-}
-void black(){
-	printf("\033[0;30m");
-}
-void red(){
-	printf("\033[1;31m");
-}
-void green(){
-	printf("\033[1;32m");
-}
-void yellow(){
-	printf("\033[1;33m");
-}
-void blue(){
-	printf("\033[1;34m");
-}
-void Bcyan(){
-	printf("\033[1;36m");
-}
-void cyan(){
-	printf("\033[0;30m");
-}
-void white(){
-	printf("\033[1;37m");
-}
-void reset(){
-	printf("\033[0m");
-}
 
 void printhelp()
 {
