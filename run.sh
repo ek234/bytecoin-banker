bd195b252904a04ebb1a4a7f82a246c8fd3a89d7
anjalish05 <77845126+anjalish05@users.noreply.github.com>
2021-05-09 14:40:08 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
  #!/bin/bash
 
- gcc main.c bitcoin.c hash.c trans.c user.c update_val.c -g -o iiitcoin.c.ex && ./iiitcoin.c.ex
+ gcc main.c bitcoin.c hash.c trans.c user.c update_val.c color.c -g -o iiitcoin.c.ex && ./iiitcoin.c.ex
