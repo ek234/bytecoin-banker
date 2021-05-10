13b4e71f5bddd94f94c7b6e34ebd251e51ca075d
Rishabh Agrawal <79305224+rishabh770@users.noreply.github.com>
2021-05-10 18:02:05 +0530

    Update main.c

--- main.c
+++ main.c
@@ -29,10 +29,10 @@ int main()
 	double bit_value = 100;
 	// struct containing overhead data about number of users and number of transactions
 	data net_data;
-	net_data.old_usr = 0;
-	net_data.new_usr = 0;
-	net_data.old_trans = 0;
-	net_data.new_trans = 0;
+    net_data.old_usr = 0;
+    net_data.new_usr = 0;
+    net_data.old_trans = 0;
+    net_data.new_trans = 0;
 	Transact head_trans = (Transact)malloc(sizeof(Transaction));
 	head_trans->next = NULL;
 
@@ -266,7 +266,7 @@ int main()
 
 					bit_value = upd_val( &net_data, bit_value );
 					double bal = delete_user(userlist, uid, bit_value);
-					if( bal==-1.0 )
+					if( bal==-1 )
 					{
 						red();
 						printf("Error: user ID doesn't exist\n");
